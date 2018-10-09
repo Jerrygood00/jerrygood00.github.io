c4b017a409c680c1367f0b7e810f5328eba4f664
Yun Che Hsieh <yunche.hsieh@sjsu.edu>
2018-10-09 10:17:37 -0700

    1

---
+++ in-class_exercise_1/sketch.js
@@ -0,0 +1,18 @@
+// declare variables here
+
+
+function setup() {
+  // put setup code here --> this runs once upon launch
+
+  createCanvas(500, 500);
+  background(0);
+}
+
+
+function draw() {
+  rect(500, 500);
+  // put drawing code here --> this loops every frame
+}
+
+
+// write custom functions here
