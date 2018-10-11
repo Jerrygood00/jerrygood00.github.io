9a22f038291ae7ec07fbaf2ccbd1b515dc1b88e4
Yun Che Hsieh <yunche.hsieh@sjsu.edu>
2018-10-11 11:06:49 -0700

    1

--- in-class_exercise_1/sketch.js
+++ in-class_exercise_1/sketch.js
@@ -16,20 +16,21 @@ function draw() {
   // put drawing code here --> this loops every frame
   background(150,0,20,0);
   fill(0, 255, 0);
-	stroke(255, 255, 255);
+	stroke(255, 255, 5);
 	ellipse(100, 100, 80, 80);
 
 	// make transparent rectangle
 	// fourth argument will define transparency
 	// 125 is 50% transparency (half of 255)
-	fill(255, 100, 50, 127);
-	noStroke();
+	fill(0, 127);
+	// noStroke();
 	rect(100, 100, 80, 80);
 
 	// stroke color and transparency changes with mouse position
 	stroke(100, 200, mouseY, mouseX);
-	strokeWeight(5);
-	line(100, 100, mouseX, mouseY);
+	strokeWeight(random(5,100));
+	rect(mouseX, mouseY, mouseX, mouseY);
+  rectMode(CENTER);
 }
 
 
