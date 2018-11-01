023b79caf1a491fd51cc8b04c8e4ccaa7e004a2d
Yun Che Hsieh <yunche.hsieh@sjsu.edu>
2018-11-01 11:46:21 -0700

    Update in-class_exercise_1/sketch2.js

--- in-class_exercise_1/sketch2.js
+++ in-class_exercise_1/sketch2.js
@@ -23,18 +23,18 @@ function draw() {
 
   push();
 
-  translate(width/2,height/2) // shift it over
+  translate(width/2,height/2)
   fill(255);
 
   var scaledS = map(s,0,60,0,360);
   var scaledM = map(m,0,60,0,360);
 
   for (var i=0; i<300; i+=5) {
-  rect(i, i, i, 40+sin(a)*40.0);  // draw an anchor
+  rect(i, i, i, 40+sin(a)*40.0);
   stroke (random(tan(a)*200.0 - 100));;
   strokeWeight(4);
-  rotate(radians(s*6)); //rotate on the shift
-  translate(40,40); // shift from anchor
+  rotate(radians(s*6));
+  translate(40,40);
   scale(.99,.99);
   a = a + inc;
     inc = -inc;
@@ -44,15 +44,15 @@ function draw() {
  pop();
 //////////////////////////
  push();
-  translate(width/2,height/2); // shift it over
+  translate(width/2,height/2);
   fill(random(cos(a)*300.0 - 100));
 
   for (var i=0; i<300; i+=5) {
-  ellipse(i, 60, i, 60+cos(a)*60.0);  // draw an anchor
+  ellipse(i, 60, i, 60+cos(a)*60.0);
   stroke (random(cos(a)*300.0 - 100));
   strokeWeight(4);
-  rotate(radians(m*6)); //rotate on the shift
-  translate(900,40); // shift from anchor
+  rotate(radians(m*6));
+  translate(900,40);
   scale(1,1);
   a = a + inc;
     inc = -inc;
